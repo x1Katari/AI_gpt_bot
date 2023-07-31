c85c47e91b2045d140aa9ee682effb45ea39219c
Dinar Gaskarov <72319713+x1Katari@users.noreply.github.com>
2023-08-01 02:20:17 +0400

    Update script.js

--- src/backend/templates/script.js
+++ src/backend/templates/script.js
@@ -8,22 +8,10 @@ document.querySelectorAll('.card').forEach(card => {
 
 function sendData(characterId) {
     fetch(`/set-character?character_id=${characterId}&user_id=${window.Telegram.WebApp.initDataUnsafe.user.id}`, {
-      method: 'POST',
-      // headers: {
-      //   'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-      //   'Connection': 'keep-alive',
-      //   'Content-Length': '0',
-      //   'Origin': 'http://127.0.0.1:8001',
-      //   'Referer': 'http://127.0.0.1:8001/docs',
-      //   'Sec-Fetch-Dest': 'empty',
-      //   'Sec-Fetch-Mode': 'cors',
-      //   'Sec-Fetch-Site': 'same-origin',
-      //   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-      //   'accept': 'application/json',
-      //   'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-      //   'sec-ch-ua-mobile': '?0',
-      //   'sec-ch-ua-platform': '"macOS"'
-      // }
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
 });
     
     // const data = {
