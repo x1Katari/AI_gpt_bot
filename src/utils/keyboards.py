de1fbf2fc3c49fa59ae4e0a123cefe5beb7e769b
Dinar Gaskarov <72319713+x1Katari@users.noreply.github.com>
2023-08-01 01:55:02 +0400

    Update keyboards.py

--- src/utils/keyboards.py
+++ src/utils/keyboards.py
@@ -3,4 +3,4 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 def get_web_app_markup():
     return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Выбрать персонажа', web_app=WebAppInfo(
-url=f'https://amadeusx.ru/choose-character'))]])
+url=f'https://amadeusx.ru/ai/choose-character'))]])
